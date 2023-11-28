28164beb905944299267bae94fb670a993b45e1a
pwntheweb <ezra005@proton.me>
2023-11-28 23:13:57 +0000

    new added

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if (i is 8 and j is 9):
-            print("{}".format(str(i) + str(j)))
-        elif (j > i):
-            print("{}".format(str(i) + str(j)) + ", ", end='')
+n = 1
+for i in range(0, 8):
+    for j in range(n, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+        n = n + 1
+print("89")
