9b09660c92fabce6cc6babcbda9de59360321d3c
pwntheweb <ezra005@proton.me>
2023-12-04 12:40:24 +0000

    idk what just happened

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
+            print("{:d}".format(num), end="")
+            if (num is not row[len(row)-1]):
+                print(" ", end="")
         print()
