8149b385d74efc5bf91376524489903d9174b178
pwntheweb <ezra005@proton.me>
2023-12-06 15:06:05 +0000

    plan and execute

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sortedKeys = sorted(a_dictionary.keys())
+    for key in sortedKeys:
+        print(f"{key}: {a_dictionary[key]}")
