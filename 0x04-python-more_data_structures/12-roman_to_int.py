30158b18d1930b64162bf39d6b7a5fbb94bc7790
pwntheweb <ezra005@proton.me>
2023-12-06 18:15:29 +0000

    rome isn't built in a day

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    romanValue = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prevValue = 0
+    for symbol in reversed(roman_string):
+        value = romanValue.get(symbol, 0)
+        if value >= prevValue:
+            result += value
+        else:
+            result -= value
+        prevValue = value
+    return result
