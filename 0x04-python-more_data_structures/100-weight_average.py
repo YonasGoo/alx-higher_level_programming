a76c2f1b38f7c1037a962266ee3b8841d3dfec92
pwntheweb <ezra005@proton.me>
2023-12-06 18:21:12 +0000

    sacrifice a lot

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    totalScore = 0
+    totalWeight = 0
+    for score, weight in my_list:
+        totalScore += score * weight
+        totalWeight += weight
+    if totalWeight != 0:
+        return totalScore / totalWeight
+    else:
+        return 0
