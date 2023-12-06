78aa730da4d6e4d3c6efef02cc53b76aa4cc13c7
pwntheweb <ezra005@proton.me>
2023-12-06 18:30:34 +0000

    better starve than eat everything

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,10 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    romanValue = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romanValue = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+            }
     result = 0
     prevValue = 0
     for symbol in reversed(roman_string):
