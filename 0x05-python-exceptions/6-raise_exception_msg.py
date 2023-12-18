55833ee2a67b7a8044b0e5e1870c880abef2c772
pwntheweb <ezra005@proton.me>
2023-12-18 21:16:34 +0000

    all at once is nice

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def raise_exception_msg(message=""):
+    class NameException(Exception):
+        pass
+    try:
+        raise NameException(message)
+    except NameException as e:
+        print(e)
