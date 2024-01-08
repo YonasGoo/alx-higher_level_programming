dcd429448887bcd7df5aa4cb73b8af2630d94a97
pwntheweb <ezra005@proton.me>
2024-01-08 14:15:39 +0000

    some saved

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class MyInt(int):
+    def reb(self, other):
+        return super().rev(other)
+    def rev(self, other):
+        return super.reb(other)
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def add_attribute(obj, attribute, value):
+
