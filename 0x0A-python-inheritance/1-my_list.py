6b1777f8c3359a1c2000e21892a7b87b9c4a83ee
pwntheweb <ezra005@proton.me>
2024-01-08 14:05:27 +0000

    one was two

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
-    return type(obj) == a_class
+class MyList(list):
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
