50cae426253c44a02869e49dda0c5b900990c71e
pwntheweb <ezra005@proton.me>
2024-01-11 01:53:16 +0000

    style

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that returns attributes and methods of an object"""
+
+
 def lookup(obj):
     attributes_methods = dir(obj)
     return list(attributes_methods)
