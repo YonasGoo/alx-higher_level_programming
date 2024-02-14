d2bdf938f2c231156968633706588b2734969e2c
pwntheweb <ezra005@proton.me>
2024-02-14 11:40:38 +0000

    comedy_only JOIN fix

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- lists all comedy shows in the database
 SELECT ts.title FROM tv_show AS ts
+JOIN tv_show_genres AS tgs
 On ts.id = tgs.show_id
-JOIN ts.id = tgs.show_id
 JOIN tv_genres AS tgg
 ON tgs.genre_id = tgg.id
 WHERE tgg.name = 'Comedy'
