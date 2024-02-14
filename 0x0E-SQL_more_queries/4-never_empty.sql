7d9c094ff262f1e20256fe3b6f3f2aa9a8443cc3
pwntheweb <ezra005@proton.me>
2024-02-14 11:32:18 +0000

    empty updated

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- creates table
-CREATE TABLE IF NOT EXISTS 'id_not_null'
-(id, INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `id_not_null`
+(`id`, INT DEFAULT 1, `name` VARCHAR(256));
