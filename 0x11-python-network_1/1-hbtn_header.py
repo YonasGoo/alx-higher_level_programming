0f3f2740eefc883376b98e19c1f81d3e1e738df2
pwntheweb <ezra005@proton.me>
2024-03-29 21:39:49 +0000

    url sender

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,6 @@
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
-    x_request_id = response.getheader('X-Request-Id')
-    print(x_request_id)
-
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
