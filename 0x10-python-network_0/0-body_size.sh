0df9d029534e8890fd784c07d011923559e52fee
pwntheweb <ezra005@proton.me>
2024-03-29 22:44:55 +0000

    simple as this?

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# takes and sends url
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-
-RESPONSE_SIZE=$(curl -s -o /dev/null -w "%{size_download}" "$URL")
-
-echo "$RESPONSE_SIZE"
+# curl body size
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
