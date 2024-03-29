7a31d5f96482e571abfee5a2c38dc5872903a3c5
pwntheweb <ezra005@proton.me>
2024-03-29 21:36:52 +0000

    feteches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 # fetches web
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        content = response.read()
-        url = content.decode('utf-8')
-        typed = response.info()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(url))
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    utf8_content = body.decode('utf-8')
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", utf8_content)
