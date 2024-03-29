6a9fe52e8e7ee4accad1f3588de1f21d4d03b9d7
pwntheweb <ezra005@proton.me>
2024-03-29 21:53:07 +0000

    removed parse

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-# sends request
+# sends url request
 import urllib.request
-import urlib.parse
 import urllib.error
 import sys
 
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-import urlib.parseif __name__ == "__main__":
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            thisPage = response.read().decode('utf-8')
-            print(thisPage)
-    except urllib.error.URLError as e:
-        print("Error code: {}".format(e.code))
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
+
