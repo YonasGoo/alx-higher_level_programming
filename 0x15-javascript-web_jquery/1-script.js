c8bf6141fd3e1fc6a2124a7ad715f71b5011e921
Pwntheweb <ezra005@proton.me>
2024-05-10 05:39:48 +0300

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/1-script.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    $('HEADER').css('color', '#FF0000');
-  });
--- 0x15-javascript-web_jquery/8-script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $.getJSON('https://swapi.co/api/films/?format=json', function (data) {
-      $.each(data.results, function (index, value) {
-        $('UL#list_movies').append('<li>' + value.title + '</li>');
-      });
-    });
-  });
