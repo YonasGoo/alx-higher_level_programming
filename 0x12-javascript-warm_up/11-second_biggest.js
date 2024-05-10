587fc1e2c78fff077e0901ad55e7be416634e9eb
Pwntheweb <ezra005@proton.me>
2024-05-10 05:38:25 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-let nums = process.argv.slice(2).map(x => Number(x));
-if (nums.length <= 1) {
-  console.log(0);
-} else {
-  console.log(nums.sort().reverse()[1]);
-}
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-let num1 = process.argv[2];
-let num2 = process.argv[3];
-function add (a, b) {
-  return (a + b);
-}
-
-console.log(add(Number(num1), Number(num2)));
