e1e9c41bd4e34b79903ac799fbb358e7af2022b3
Zyonasec <ezra005@proton.me>
2024-06-20 18:15:27 +0300

    biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+function findBig(args) {
+	let nums = args.map(arg => parseInt(arg)).filter(num => !isNaN(num));
+	if (nums.length === 0 || nums.length === 1) {
+		console.log(0);
+	} else {
+		let maxNum = Math.max(...nums);
+		console.log(maxNum);
+	}
+}
+let input = process.argv.slice(2);
+findBig(input);
