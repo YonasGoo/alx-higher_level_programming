a07132ddf92ce386186378acda3c1e7556b5f4f0
Zyonasec <ezra005@proton.me>
2024-06-20 16:43:11 +0300

    c is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+function printMsg(times) {
+	let num = parseInt(times);
+	if (isNaN(num)) {
+		console.log('Missing number of occurrences');
+	} else {
+		for (let i = 0; i < num; i++) {
+			console.log('C is fun');
+		}
+	}
+}
+let input = process.argv[2];
+printMsg(input);
