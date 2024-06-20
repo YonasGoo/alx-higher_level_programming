cda227d33fb1bb19137e03a665a80a8a70a728e9
Zyonasec <ezra005@proton.me>
2024-06-20 16:58:24 +0300

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+function printSquare(size) {
+	let num = parseInt(size);
+	if (isNaN(num)) {
+		console.log('Missing size');
+	} else {
+		for (let i = 0; i < num; i++) {
+			let row = '';
+			for (let j = 0; j < num; j++) {
+				row += 'X';
+			}
+			console.log(row);
+		}
+		
+	}
+}
+let input = process.argv[2];
+printSquare(input);
