c2bf72d4dbf385eccc1e7bd05f46bbdad9509340
Zyonasec <ezra005@proton.me>
2024-06-20 17:20:23 +0300

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+function add(a, b) {
+	let num1 = parseInt(a);
+	let num2 = parseInt(b);
+	if (isNaN(num1) || isNaN(num2)) {
+		console.log(NaN);
+	} else {
+	let sum = num1 + num2;
+	console.log(sum);
+}
+}
+let input1 = process.argv[2];
+let input2 = process.argv[3];
+add(input1,input2);
