f12315838fe9575171e86eb71490f11a23d2ea3a
Zyonasec <ezra005@proton.me>
2024-06-20 17:37:36 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+function factorial(n) {
+	let num = parseInt(n);
+	if (isNaN(num)) {
+		console.log(1);
+		return;
+	}
+	if (num === 0) {
+		return 1;
+	} else {
+		return num * factorial(num -1);
+		console.log(num);
+	}
+}
+let input = process.argv[2];
+let result = factorial(input);
+if (result !== undefined) {
+	console.log(result);
+}
