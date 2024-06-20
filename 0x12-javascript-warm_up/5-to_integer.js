68419b0bd4e8fa1f0ccb9b3eb951138d10598927
Zyonasec <ezra005@proton.me>
2024-06-20 15:51:03 +0300

    wrapper

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+function convertNum(value) {
+	let num = Number(value);
+	if (isNaN(num)) {
+		console.log('Not a Number');
+	} else {
+		console.log('My number:', num);
+	}
+}
+let input = process.argv[2];
+convertNum(input);
