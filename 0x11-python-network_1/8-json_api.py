f86b9f58903ea12d45b4200c2e557bc1b177e1a3
Zyonasec <ezra005@proton.me>
2024-07-05 14:56:05 +0300

    network 2

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# search API
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    payload = {'q': q}
+    url = 'http://0.0.0.0:5000/search_user'
+
+    response = requests.post(url, data=payload)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
