3815f14ee95165dffcd23fe96a68bca41109a6f3
Zyonasec <ezra005@proton.me>
2024-07-05 15:21:51 +0300

    post mail

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,14 +5,9 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('utf-8')
-
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+    val = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(val).encode('ascii')
+    handler = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(handler) as response:
+        thisPage = response.read().decode('utf-8')
+        print(thisPage)
