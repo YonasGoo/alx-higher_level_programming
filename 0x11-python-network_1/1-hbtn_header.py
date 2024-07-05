036308ed81b8e0b839542f9358625140a0f1c73e
Zyonasec <ezra005@proton.me>
2024-07-05 15:04:07 +0300

    hedaer

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,5 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        x_request_id = response.headers.get('X-Request-Id')
-        print(x_request_id)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
