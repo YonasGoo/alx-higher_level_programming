0da6dc552f4dd488eed42d03c56f6e3e93c248dc
Zyonasec <ezra005@proton.me>
2024-07-05 15:26:57 +0300

    github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,5 +5,7 @@ from sys import argv
 try:
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     print(r.json().get('id'))
-except:
-    print("None")
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
+except IndexError:
+    print("Error: Please provide both username and password as arguments.")
