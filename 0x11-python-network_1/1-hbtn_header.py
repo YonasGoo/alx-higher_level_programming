cc39fe9e4ffcfb60f9d13da3786d750e8d17bf49
Zyonasec <ezra005@proton.me>
2024-07-07 07:46:03 +0300

    comment

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# takes and sends url
+"""Takes in a URL, sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response"""
+
 import urllib.request
 import sys
 
